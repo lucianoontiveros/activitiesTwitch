8d18b72be604f0fee9c96ab8460f5676a78409f9
lucianoontiveros <luciano.a.ontiveros@gmail.com>
2023-02-06 15:43:19 -0300

    Se ingresa nuevas imagenes caracteristicas del canal

--- src/components/MensajeEmergente.jsx
+++ src/components/MensajeEmergente.jsx
@@ -15,7 +15,7 @@ const MensajeEmergente = (props) => {
         props.showMessage && (
           <a
             href="#"
-            className="cardImg flex flex-col items-center bg-black border border-green-200 rounded-lg shadow md:flex-row hover:bg-green-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+            className="cardImg flex flex-col items-center border border-green-200 rounded-lg shadow md:flex-row hover:bg-green-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
           >
             <img className="carimgH object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={pomo} />
             <div id="cuerpoAlerta" className="flex flex-col justify-between p-4 leading-normal">
